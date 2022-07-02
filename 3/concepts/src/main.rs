aeda94d8c59109c040e73d38504f2a61c3398d2d
MB <michael.dburke56@gmail.com>
2022-07-02 08:30:53 -0500

    Complete chapter 3.

--- 3/concepts/src/main.rs
+++ 3/concepts/src/main.rs
@@ -22,11 +22,47 @@ fn main() {
 
     let byte = [0; 0];// create array len 8 w/ all values == 0
 
-    my_function(11, 22);
+    let sum = my_function(11, 22);
+    println!("The sum is: {}", sum);
+    
+    loops();
 }
 
 
-fn my_function(x :i32, y :i32) {
+fn my_function(x :i32, y :i32) -> i32 {
     println!("Passed in vars: {}", x);
     println!("Passed in vars: {}", y);
+    x + y
+}
+
+
+fn loops() {
+    let mut counter = 0;
+
+    let result = loop {
+         counter += 1;
+         if counter == 10 {
+         break counter; //returns counter
+         }
+    };
+
+    println!("The result is {}", result);
+
+    //While loop 
+    let mut num = 3;
+    while num != 0 {
+        num -= 1;
+    }
+
+    //For in loop
+    let a = [1, 2, 3];
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    //For in using range 
+    for number in 1..4 { // last param in range is exclusive -> 1 2 3
+        println!("the value is: {}", number);
+    }
+
 }
