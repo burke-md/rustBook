b2bd1c288cbe4620bc5cde9b6708edbf400a0e98
MB <michael.dburke56@gmail.com>
2022-07-02 21:40:39 -0500

    Note the rules of rust ownership.

--- 4/ownership/src/main.rs
+++ 4/ownership/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    // Ownership rules
+    // 1 Each value in Rust has a variable thats called its owner
+    // 2 there can only be one owner at a time
+    // 3 when the owner goes out of scope the value will be dropped
 }
